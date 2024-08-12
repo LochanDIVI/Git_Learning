aed997bb961a3510aa204f27440ce7f22452c4f4
LochanDIVI <divilochan@gmail.com>
2024-08-12 19:10:34 +0530

    Added new folder

---
+++ Mypython.py
@@ -0,0 +1,31 @@
+# Learning Python Programming
+
+# To print any statements with in " " or ' ' or ''' ''' or """ """ Examples are below
+print("-----Something's to Learn-----")    
+print('-----To Learn "Python" for my future-----')
+print('''Practicing python
+      to improve
+      Myself''')
+print("""To print paragraph's
+      and things that has to print
+      in "multiple" line's""")
+# To print any statements with Values, Examples are below
+# Python is implicit typed language
+#that means no need to mention the type of variable like String name1="Sri Krishna")
+
+name1="Sri Krishna"
+age=21
+cookie_price=74.36
+value1=True
+# name1, age, cookie_price are examples of variable name's to assign various type of values
+# variable name can't be start with numeric value(like 1name)
+# apart for (a-z) or (A-Z) or (_) or combination of upper and lower case can be used as a Variables or identifiers
+print("My name is",name1)
+print("My age is",age)
+print("I have",cookie_price,"amount to buy this cookie")
+
+# Data types in python
+print(type(name1))
+print(type(age))
+print(type(cookie_price))
+print(type(value1))
